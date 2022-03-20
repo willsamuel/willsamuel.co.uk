ff28901eea7df3c4badc46f25b636ec4736ce161
Will Samuel <mail@willsamuel.co.uk>
2022-03-20 19:43:04 +0000

    first work

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,49 +11,29 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div class="home-inner">
+		<img src="bill-cropped.jpg"/>
+		<h1>Will Samuel - Developer</h1>
+	</div>
 </section>
 
 <style>
 	section {
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		flex: 1;
+		justify-content: center;
+		background-color: rgba(255,255,255,0.5);
+		padding: 20px;
 	}
 
-	h1 {
-		width: 100%;
+	.home-inner {
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.home-inner img {
+		width: 300px;
+		margin-right: 10px;
 	}
 </style>
